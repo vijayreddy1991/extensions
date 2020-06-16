5f23faefe4a141e991fb9adbccf258ab20648618
Bharath Upadhya <bharathu@jfrog.com>
2020-06-16 19:39:47 +0530

    x

--- resources/sampleResource/0.0.1/onInput.sh
+++
@@ -1,9 +0,0 @@
-sampleResource001() {
-  local resourceName="$1"
-  local resourceFoo=$(find_resource_variable "$resourceName" "resourceFoo")
-  local resourceIntegration=$(find_resource_variable "$resourceName" "integrationName")
-  echo "resourceFoo: $resourceFoo"
-  echo "resourceIntegration: $resourceIntegration"
-}
-
-execute_command "sampleResource001 %%context.resourceName%%"
--- steps/sampleStep/0.0.2/onExecute.sh
+++
@@ -1,6 +0,0 @@
-sampleStepType002() {
-  local foo=$(find_step_configuration_value "foo")
-  echo "foo: $foo"
-}
-
-execute_command "sampleStepType002"
