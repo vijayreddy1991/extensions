7e7b4da4da9bf3db84d22f8482ecc9c0624e9bd2
Bharath Upadhya <bharathu@jfrog.com>
2020-06-16 09:46:42 +0530

    demo

--- resources/sampleResource001/0.0.1/onInput.sh
+++
@@ -1,7 +0,0 @@
-sampleResource001() {
-  local resource="%%context.resourceName%%"
-  local foo=$(find_resource_variable "$resource" "resourceFoo")
-  echo "foo: $foo"
-}
-
-execute_command "sampleResource001 %%context.resourceName%%"
