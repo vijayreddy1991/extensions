f82c191576b913264eecc4573f7e5ff39d3fa33d
Bharath Upadhya <bharathu@jfrog.com>
2020-07-16 20:25:53 +0530

    test

--- resources/namespace1/sampleResource/onInput.sh
+++
@@ -1,7 +0,0 @@
-sampleResource001() {
-  local resourceName="$1"
-  local resourceFoo=$(find_resource_variable "$resourceName" "resourceFoo")
-  echo "resourceFoo: $resourceFoo"
-}
-
-execute_command "sampleResource001 %%context.resourceName%%"
