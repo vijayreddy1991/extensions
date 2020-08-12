0c7cceeff537a3836d8a38811816c1cc5f0b34f6
Bharath Upadhya <bharathu@jfrog.com>
2020-08-12 15:16:10 +0530

    adds more extesnions

--- resources/qhode/GitRepo/onInput.sh
+++ resources/qhode/GitRepo/onInput.sh
@@ -2,6 +2,8 @@ sampleResource001() {
   local resourceName="$1"
   local resourceFoo=$(find_resource_variable "$resourceName" "resourceFoo")
   echo "resourceFoo: $resourceFoo"
+
+  node GitRepo.js
 }
 
 execute_command "sampleResource001 %%context.resourceName%%"
